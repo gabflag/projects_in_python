dac369c732ceb6cb5115a24f4ca48dd450ae6ce2
Gabriel <95552879+gabflag@users.noreply.github.com>
2022-03-12 14:40:40 -0300

    Update meu_onibus.py

--- programas_fixacao_conteudo/meu_onibus/meu_onibus.py
+++ programas_fixacao_conteudo/meu_onibus/meu_onibus.py
@@ -34,13 +34,5 @@ def main():
   hora_atual = 15
   minuto_atual = 25
   hora_formatada = timedelta(hours= hora_atual, minutes= minuto_atual)
-
-  part_bairro_jose_nt = ['05:45', '06:00', '06:35', '06:45', '07:00', '07:40', '08:35', '09:45', '11:10', '12:05', '13:10', '14:00', '15:20', '16:40', '17:50', '19:15', '22:35']
-  part_centro_jose_nt = ['06:15', '06:20', '07:00', '07:55', '09:10', '10:30', '11:25', '12:30', '13:20', '14:40', '16:00', '17:10', '18:25', '19:40', '21:00', '22:00', '23:30']
-
-  index = retorna_proximo(part_centro_jose_nt  , hora_formatada)
-
-  print(part_centro_jose_nt[index])
-
-
+  
 main()
